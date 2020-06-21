6b214c72274e1ad0ebbd60bb20dfe2663b3a5028
metacoder87 <gary.miller@gmx.com>
2020-06-20 20:49:06 -0400

    Creates the minotaur_spec file

---
+++ spec/minotaur_spec.rb
@@ -0,0 +1,8 @@
+            # meta_coder (Gary Miller) =)
+            # gmiller052611@gmail.com
+            # https://github.com/metacoder87/maze_solver
+
+require 'minotaur'
+
+describe "Maze" do
+    let(:maze) { Maze.new('maze.txt') }
